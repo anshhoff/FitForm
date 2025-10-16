34d82aa738519c5eb532cc8967cbd6c262cbe510
anshhoff <anshhoff@github.com>
2025-10-16 12:50:52 +0530

    Initial Commit

---
+++ FitForm/FitFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFormApp.swift
+//  FitForm
+//
+//  Created by Ansh Hardaha on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FitFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
